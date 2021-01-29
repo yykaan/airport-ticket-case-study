43ebb90100ce50bd5305ceaaa1902e46a0bf7c9c
kaan <kaanyilmaz@anayurtteknoloji.com>
2021-01-29 10:41:01 +0300

    Initial commit1

---
+++ src/main/java/com/kaan/finartz/airportticket/AirportticketApplication.java
@@ -0,0 +1,13 @@
+package com.kaan.finartz.airportticket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirportticketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirportticketApplication.class, args);
+	}
+
+}
